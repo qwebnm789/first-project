2e2d6280ef83bb4ed722963188fc49fe951915c9
Yeongchae Kim <qwebnm789@naver.com>
2021-12-29 20:14:30 +0900

    first commit

---
+++ 4_2_text.py
@@ -0,0 +1,12 @@
+print('Hello World')
+print("Hello World")
+print('''
+Hello
+World Hello
+World Hello World
+''')
+
+print("'1'+'1'", '1'+'1')
+print('Hi'*100)
+print("len('Hi'*100)", len('Hi'*100))
+print("'Hello world'.replace('world', 'universe')", 'Hello world'.replace('world', 'universe'))
